5db5dfecc463838db8fb44e63e1bb7c5f99de1f3
Niall Curtis <CurtisNS@cardiff.ac.uk>
2018-02-01 18:47:17 +0000

    Mostly done, still awaiting displaying collection on client

--- client/main.js
+++ client/main.js
@@ -1,14 +1,17 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+import { People } from '/common/people.js';
 
 import './main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+  Meteor.subscribe('allPeople');
 });
 
 Template.hello.helpers({
+  getPeople() {
+    return People.publicFields;
+  },
   counter() {
     return Template.instance().counter.get();
   },
@@ -16,7 +19,11 @@ Template.hello.helpers({
 
 Template.hello.events({
   'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
+    // Take the user's name from input box
+    const name = instance.$('[name="name"]').val();
+    // Take the user's age from input box
+    const age = instance.$('[name="age"]').val();
+    // Call the 'addPerson' method on the server
+    Meteor.call('addPerson', name, age);
   },
 });
--- server/main.js
+++ server/main.js
@@ -1,5 +1,18 @@
 import { Meteor } from 'meteor/meteor';
+import { People } from '/common/people.js';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  // Server methods
+  Meteor.methods({
+    // Add the person with the 'name' and 'age' parameters from the web page to the MongoDB instance
+    addPerson(name, age) {
+      People.insert({name: name, age: age});
+    }
+  })
+  Meteor.publish('allPeople', function() {
+    // Return all public lists when the client is subscribed
+    return People.find({
+      fields: People.publicFields
+    });
+  })
 });
